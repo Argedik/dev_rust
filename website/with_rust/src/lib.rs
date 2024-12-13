52b0c5bef9f6a52e343d48f6af1232ee201588c1
Argedik <gedikas@hotmail.com>
2024-12-13 08:14:21 +0300

    Fixed errors

--- website/with_rust/src/lib.rs
+++ website/with_rust/src/lib.rs
@@ -1,47 +1,12 @@
-use yew::prelude::*;
+use wasm_bindgen::prelude::*;
 
-struct App {
-    name: String,
-}
-
-enum Msg {
-    UpdateName(String),
-}
-
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            name: "Ziyaretçi".to_string(),
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::UpdateName(new_name) => {
-                self.name = new_name;
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        let on_input = ctx.link().callback(|e: InputEvent| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            Msg::UpdateName(input.value())
-        });
-
-        html! {
-            <div>
-                <h1>{"Merhaba, "}{&self.name}{"!"}</h1>
-                <input type="text" oninput={on_input} placeholder="Adınızı girin" />
-            </div>
-        }
-    }
-}
-
-fn main() {
-    yew::start_app::<App>();
+#[wasm_bindgen(start)]
+pub fn run() {
+    web_sys::window()
+        .expect("window not available")
+        .document()
+        .expect("document not available")
+        .get_element_by_id("output")
+        .expect("output element not found")
+        .set_inner_html("<h1>Hello, Rust + WASM!</h1>");
 }
