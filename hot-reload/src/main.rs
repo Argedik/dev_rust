b186188f16bacf0ea092f3b38f8a53367accae42
Argedik <gedikas@hotmail.com>
2024-12-13 08:14:38 +0300

    fixed errors

--- hot-reload/src/main.rs
+++ hot-reload/src/main.rs
@@ -1,12 +1,4 @@
-#[hot_lib_reloader::hot_module(dylib = "lib")]
-mod hot_lib {
-    hot_functions_from_file!("lib/src/lib.rs");
-}
 
 fn main() {
-    loop {
-        hot_lib::example();
-
-        std::thread::sleep(std::time::Duration::from_secs(1));
-    }
+    println!("test")
 }
