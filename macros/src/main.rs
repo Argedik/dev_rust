01d096a30da026738905dd8f62ef6366baf528ae
Argedık <gedikas@hotmail.com>
2024-12-27 10:40:41 +0300

    Learning borrowing and references & / *

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,28 +1,29 @@
-macro_rules! check_even {
-    ($num:expr) => {
-        if $num % 2 == 0 {
-            println!("{} is even.", $num);
-        } else {
-            println!("{} is odd.", $num);
-        }
-    };
-}
+// macro_rules! check_even {
+//     ($num:expr) => {
+//         if $num % 2 == 0 {
+//             println!("{} is even.", $num);
+//         } else {
+//             println!("{} is odd.", $num);
+//         }
+//     };
+// }
 
-macro_rules! my_vec {
-    ( $( $x:expr ),* ) => {
-        {
-            let mut temp_vec = Vec::new();
-            $(
-                temp_vec.push($x);
-            )*
-            temp_vec
-        }
-    };
-}
+// macro_rules! my_vec {
+//     ( $( $x:expr ),* ) => {
+//         {
+//             let mut temp_vec = Vec::new();
+//             $(
+//                 temp_vec.push($x);
+//             )*
+//             temp_vec
+//         }
+//     };
+// }
 
 fn main() {
-    check_even!(10); // Çıktı: 10 is even.
-    check_even!(7);  // Çıktı: 7 is odd.
-    let v = my_vec![1, 2, 3, 4];
-    println!("{:?}", v); // Çıktı: [1, 2, 3, 4]
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    let num: &i32 = &v[0];
+    //num referansı kullanımdayken v değiştirilemez
+    v.push(4);
+    println!("{:?}", *num); // Çıktı: [1, 2, 3, 4]
 }
