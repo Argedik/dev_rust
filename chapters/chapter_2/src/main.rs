530731c0707453ef530ca2f31aa3c1627ce76ee2
Argedik <gedikas@hotmail.com>
2025-01-04 23:37:07 +0300

    Creating web site

---
+++ chapters/chapter_2/src/main.rs
@@ -0,0 +1,5 @@
+use yew::prelude::*;
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
---
+++ chapters/chapter_2/src/page1.rs
@@ -0,0 +1,8 @@
+#[function_component(first_page)]
+pub fn first_page_fn() -> Html {
+  html! {
+    <div>
+      deneme
+    <div>
+  }
+}
